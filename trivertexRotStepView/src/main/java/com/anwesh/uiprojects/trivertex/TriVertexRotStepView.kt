d36d644a4578c7a14f0b18cee1a9121000502589
Anwesh43 <anweshthecool0@gmail.com>
2018-11-23 16:20:44 +0530

    adding drawTVRNode canvas extension method

--- trivertexRotStepView/src/main/java/com/anwesh/uiprojects/trivertex/TriVertexRotStepView.kt
+++ trivertexRotStepView/src/main/java/com/anwesh/uiprojects/trivertex/TriVertexRotStepView.kt
@@ -29,3 +29,41 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + (scaleFactor()) * b.getInverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
+
+fun Canvas.drawTVRNode(i : Int, scale : Float, paint : Paint) {
+    var w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val deg : Float = 360f * tris.getInverse()
+    val size : Float = gap / sizeFactor
+    val triR : Float = size / 8
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = color
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(- 90f * sc2)
+    for (j in 0..(tris - 1)) {
+        val sc : Float = sc1.divideScale(j, tris)
+        save()
+        rotate( deg * j)
+        translate(size, 0f)
+        save()
+        rotate(90f * sc)
+        val path : Path = Path()
+        for (k in 0..(tris-1)) {
+            val d : Double = (2 * Math.PI / tris)
+            val x : Float = triR * Math.cos(k * d).toFloat()
+            val y : Float = triR * Math.sin(k * d).toFloat()
+            if (k == 0) {
+                path.moveTo(x, y)
+            } else {
+                path.lineTo(x, y)
+            }
+        }
+        drawPath(path, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
