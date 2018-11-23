abcf03e69f35ef4ada4c306c0edbf567acac364d
Anwesh43 <anweshthecool0@gmail.com>
2018-11-23 16:04:02 +0530

    adding Float and Canvas extension methods

--- trivertexRotStepView/src/main/java/com/anwesh/uiprojects/trivertex/TriVertexRotStepView.kt
+++ trivertexRotStepView/src/main/java/com/anwesh/uiprojects/trivertex/TriVertexRotStepView.kt
@@ -19,3 +19,13 @@ val color : Int = Color.parseColor("#673AB7")
 val sizeFactor : Int = 3
 val scDiv : Double = 0.51
 val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(this - n.getInverse() * i, 0f)) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + (scaleFactor()) * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
