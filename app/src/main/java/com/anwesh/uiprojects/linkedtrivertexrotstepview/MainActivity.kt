11d26a21f0745347f71f7a8f65be65ba371c9799
Anwesh43 <anweshthecool0@gmail.com>
2018-11-23 17:16:00 +0530

    adding TriVertexRotStepView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtrivertexrotstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrivertexrotstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtrivertexrotstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.trivertex.TriVertexRotStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriVertexRotStepView.create(this)
     }
 }
