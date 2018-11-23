21a8892d9eb206ed081ee43d17cccf5abbce34db
Anwesh43 <anweshthecool0@gmail.com>
2018-11-23 15:55:24 +0530

    adding libraries and defining nodes

--- trivertexRotStepView/src/main/java/com/anwesh/uiprojects/trivertex/TriVertexRotStepView.kt
+++ trivertexRotStepView/src/main/java/com/anwesh/uiprojects/trivertex/TriVertexRotStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.trivertex
 /**
  * Created by anweshmishra on 23/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+
+val nodes : Int = 5
+val tris : Int = 3
+val color : Int = Color.parseColor("#673AB7")
+val sizeFactor : Int = 3
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
